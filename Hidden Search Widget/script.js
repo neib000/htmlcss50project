017b4f4e2b8a928c1ee86a18a73ae181827340a2
zuradel <53078525+zuradel@users.noreply.github.com>
2021-06-19 07:48:33 +0700

    day 4

---
+++ Hidden Search Widget/script.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('.btn');
+const input = document.querySelector('.input');
+const search = document.querySelector('.search');
+btn.addEventListener('click',()=>{
+    search.classList.toggle('active');
+    input.focus();
+});
