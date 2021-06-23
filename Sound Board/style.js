914374cb176d070f695cc19bfeb73772d29620f0
zuradel <53078525+zuradel@users.noreply.github.com>
2021-06-23 09:49:42 +0700

    day 9

---
+++ Sound Board/style.js
@@ -0,0 +1,18 @@
+const sounds = ["applause", "tada", "victory", "boo", "wrong"];
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+  btn.innerText = sound;
+  btn.addEventListener("click", () => {
+    stopSound();
+    document.getElementById(sound).play();
+  });
+  document.getElementById("buttons").appendChild(btn);
+});
+function stopSound() {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
+    song.pause();
+    song.currentTime = 0;
+  });
+}
