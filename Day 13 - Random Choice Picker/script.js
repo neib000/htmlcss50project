fe057c512ed8d097c4f0b0b31445c6d42eb508c4
zuradel <53078525+zuradel@users.noreply.github.com>
2021-08-03 17:09:51 +0700

    Day 13

--- Day 13 - Random Choice Picker/script.js
+++ Day 13 - Random Choice Picker/script.js
@@ -1,7 +1,19 @@
-const toggles = document.querySelectorAll('.faq-toggle');
+const tagsEl = document.getElementById('tags');
+const textarea = document.getElementById('textarea')
 
-toggles.forEach( toggle =>{
-    toggle.addEventListener('click', ()=>{
-        toggle.parentNode.classList.toggle('active');
-    })
+textarea.focus();
+
+textarea.addEventListener('keyup', (e)=>{
+    createTags(e.target.value)
 })
+
+function createTags(input){
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+    tagsEl.innerHTML ='';
+    tags.forEach(element => {
+        const tagEl = document.createElement('span')
+        tagEl.classList.add('tag')
+        tagEl.innerText = element
+        tagsEl.appendChild(tagEl)
+    });
+}
