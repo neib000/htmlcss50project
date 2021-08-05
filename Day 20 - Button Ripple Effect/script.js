aa327f9c0678be06f83ed7645f0af1d84ba0d586
zuradel <53078525+zuradel@users.noreply.github.com>
2021-08-05 14:28:58 +0700

    Day 20

---
+++ Day 20 - Button Ripple Effect/script.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll('.ripple')
+buttons.forEach(button =>{
+  button.addEventListener('click', (e)=>{
+    const x = e.clientX
+    const y = e.clientY
+    console.log(x,y)
+    const buttonTop = e.target.offsetTop
+    const buttonLeft = e.target.offsetLeft
+    console.log(buttonLeft,buttonTop)
+    const xInside = x - buttonLeft
+    const yInside = y - buttonTop
+    const circle = document.createElement('span')
+    circle.classList.add('circle')
+    circle.style.top = yInside +'px'
+    circle.style.left = xInside +'px'
+
+    button.appendChild(circle)
+
+    setTimeout(()=> circle.remove(), 400)
+  })
+})
