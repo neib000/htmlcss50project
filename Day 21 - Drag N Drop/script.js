3f40bb32243082db2ca42edaa91f7cbbd64a5588
zuradel <53078525+zuradel@users.noreply.github.com>
2021-08-06 05:57:59 +0700

    Day 21

---
+++ Day 21 - Drag N Drop/script.js
@@ -0,0 +1,35 @@
+const fill = document.querySelector(".fill");
+const empties = document.querySelectorAll(".empty");
+
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);
+
+for (const empty of empties) {
+    console.log(empty)
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
+}
+
+function dragStart() {
+  this.className += ' hold'
+  setTimeout(()=>{this.className = 'invisible'},0 )
+}
+function dragEnd() {
+    this.className = 'fill'
+}
+function dragOver(e) {
+   e.preventDefault()
+}
+function dragLeave() {
+    this.className = 'empty'
+}
+function dragDrop() {
+  this.className = 'empty'
+  this.append(fill)
+}
+function dragEnter(e) {
+    e.preventDefault()
+    this.className += ' hovered'
+}
